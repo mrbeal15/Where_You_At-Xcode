27a3224f4aae59bf051fcc927d6960da812f52f8
Nicole Tolch <email": null>
2015-11-19 09:11:46 -0600

    Initialize repo

---
+++ Where You At/DataViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DataViewController.swift
+//  Where You At
+//
+//  Created by Apprentice on 11/18/15.
+//
+//
+
+import UIKit
+import CoreLocation
+
+class DataViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
